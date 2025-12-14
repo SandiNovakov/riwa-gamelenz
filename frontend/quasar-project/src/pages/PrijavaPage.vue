cb72facdf1e4537463aef072a368fecefc1aa391
goofy812 <bijeliccurkictomislav2@gmail.com>
2025-12-14 20:49:36 +0100

    Druga straanica

--- frontend/quasar-project/src/pages/PrijavaPage.vue
+++ frontend/quasar-project/src/pages/PrijavaPage.vue
@@ -9,13 +9,13 @@
       <q-form @submit="handleLogin">
 
         <q-input
-          v-model="login.username"
+          v-model="login.korisnicko_ime"
           label="Korisničko ime"
           filled
         />
 
         <q-input
-          v-model="login.password"
+          v-model="login.lozinka"
           label="Lozinka"
           type="password"
           filled
@@ -24,14 +24,37 @@
 
         <q-btn
           type="submit"
-          label="Registriraj se"
+          label="Prijava"
           color="secondary"
           class="q-mt-lg full-width"
         />
 
       </q-form>
-
     </q-card>
   </q-page>
-
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import { api } from 'src/boot/axios'
+
+const login = ref({
+  korisnicko_ime: '',
+  lozinka: ''
+})
+
+async function handleLogin () {
+  await loginUser()
+}
+
+async function loginUser () {
+  try {
+    const res = await api.post('/korisnici/:id',
+    login.value
+    )
+    console.log('Korisnik prijavljen:', res.data)
+  } catch (err) {
+    console.error('API ERROR:', err)
+  }
+}
+</script>
