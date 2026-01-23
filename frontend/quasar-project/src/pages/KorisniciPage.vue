f26c15216b179a6d847c9e264848b9aed7e98b83
tomass17 <tomas.grgic17@gmail.com>
2026-01-23 18:04:11 +0100

    tomas2

--- frontend/quasar-project/src/pages/KorisniciPage.vue
+++ frontend/quasar-project/src/pages/KorisniciPage.vue
@@ -1,9 +1,59 @@
 <template>
-  <q-page padding>
-    <!-- content -->
+  <q-page padding class="q-pa-lg">
+    <div class="text-h5 q-mb-md">Pretraga korisnika</div>
+
+    <div class="row q-col-gutter-md items-center">
+      <div class="col">
+        <q-input
+          filled
+          v-model="korisnicko_ime"
+          label="Upiši korisničko ime"
+          
+        />
+      </div>
+
+      
+        <q-btn
+          label="Traži"
+          color="primary"
+          @click="fetchUsers"
+        />
+    </div>
+
+    <div  class="q-mt-lg q-gutter-md">
+      <q-card
+        v-for="user in users"
+        :key="user.id_korisnika"
+      >
+        <q-card-section>
+          <div class="text-h6">{{ user.korisnicko_ime }}</div>
+        </q-card-section>
+      </q-card>
+    </div>
+
+    
   </q-page>
 </template>
 
 <script setup>
-//
+import { ref } from "vue";
+import { api } from "boot/axios";
+
+
+const korisnicko_ime = ref("");
+
+
+const users = ref([]);
+
+const fetchUsers = async () => {
+  try {
+    const params = { korisnicko_ime: korisnicko_ime.value };
+    const query = new URLSearchParams(params).toString();
+    const res = await api.get(`/korisnici?${query}`);
+    users.value = res.data;
+  } catch (err) {
+    console.error(err);
+    users.value = [];
+  }
+};
 </script>
